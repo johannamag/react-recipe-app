e683d305cc76a200b6e2ea34e1d9df9b95aa91ff
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-21 16:54:33 +0200

    Added API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const apiId = "a5ea7377";
+  const apiKey = "163be883c7ec91ffbcda762a03403b06";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${apiId}&app_key=${apiKey}`
+    );
+    const data = response.json();
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-btn" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
