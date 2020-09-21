1e0b9d9d7c8aed31c6be5682070d321a4a62ade7
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-21 19:43:00 +0200

    Added basic styles

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,16 @@
 import React from "react";
+import style from "./recipe.module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <ol>
+    <div className={style.recipe}>
+      <h1 className={style.title}>{title}</h1>
+      <ol className={style.ingredlist}>
         {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
       </ol>
-      <p>{calories}</p>
-      <img src={image} />
+      <img className={style.image} src={image} />
     </div>
   );
 };
